9dd6addf8223c466f4aded586cb0d49fd77d0136
Douglas Guimarães <douglas.guimaraes.2120@gmail.com>
2024-10-03 19:58:36 -0300

    change basePath

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,7 @@ const nextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "/n",
+  basePath: "/",
 
   /**
    * Disable server-based image optimization. Next.js does not support
